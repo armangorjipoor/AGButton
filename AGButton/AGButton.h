ec79f3b61b7a0f90236755631e328380f59d33c1
armangorjipoor <armangorjipoor@gmail.com>
2023-01-29 00:02:04 +0330

    init commit

---
+++ AGButton/AGButton.h
@@ -0,0 +1,18 @@
+//
+//  AGButton.h
+//  AGButton
+//
+//  Created by Arman Gorj on 11/7/1401 AP.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AGButton.
+FOUNDATION_EXPORT double AGButtonVersionNumber;
+
+//! Project version string for AGButton.
+FOUNDATION_EXPORT const unsigned char AGButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGButton/PublicHeader.h>
+
+
---
+++ AGButton_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AGButton_Example
+//
+//  Created by Arman Gorj on 11/8/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
